70e7a43eb5a1a807ebfc341f838c88d80ef2b69c
shotaro57 <qqs74h8d@gmail.com>
2018-11-03 05:54:20 +0900

    かなりおおざっぱなUIを設定した

--- app/src/main/java/com/example/user/calcprobability/MainActivity.java
+++ app/src/main/java/com/example/user/calcprobability/MainActivity.java
@@ -1,13 +1,101 @@
 package com.example.user.calcprobability;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int tmpNumerator = 0;
+    private int tmpDenominator = 0;
+    private int sumNumerator = 0;
+    private int sumDenominator = 0;
+
+    private TextView sumProbability;
+    private TextView tmpProbability;
+
+    private Button buttonNumeratorAdd;
+    private Button buttonDenominatorAdd;
+    private Button buttonNumeratorSub;
+    private Button buttonDenominatorSub;
+    private Button update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 縦画面
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        sumProbability = (TextView)findViewById(R.id.sumProbability);
+        displaySum();
+        tmpProbability = (TextView)findViewById(R.id.tmpProbability);
+        displayUI();
+
+        // ボタンを設定
+        buttonNumeratorAdd = findViewById(R.id.buttonNumeratorAdd);
+        buttonNumeratorAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tmpNumerator++;
+                displayUI();
+            }
+        });
+
+        buttonDenominatorAdd = findViewById(R.id.buttonDenominatorAdd);
+        buttonDenominatorAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tmpDenominator++;
+                displayUI();
+            }
+        });
+
+        buttonNumeratorSub = findViewById(R.id.buttonNumeratorSub);
+        buttonNumeratorSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tmpNumerator--;
+                displayUI();
+            }
+        });
+
+        buttonDenominatorSub = findViewById(R.id.buttonDenominatorSub);
+        buttonDenominatorSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tmpDenominator--;
+                displayUI();
+            }
+        });
+
+        update = findViewById(R.id.update);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumNumerator += tmpNumerator;
+                sumDenominator += tmpDenominator;
+                displaySum();
+            }
+        });
+
+    }
+
+    private void displayUI(){
+        tmpProbability.setText(tmpNumerator + "/" + tmpDenominator + "");
+    }
+
+    private void displaySum(){
+        double probability;
+
+        if(sumDenominator == 0){
+            probability = 0.0;
+        }else{
+            probability = (double)sumNumerator/(double)sumDenominator*100.0;
+        }
+        sumProbability.setText("合計：" + sumNumerator + "/" + sumDenominator + "     " + String.format("%.2f", probability) + "%");
     }
 }
